3a10416346ce6f8aff0cb1c777a172ef121776b9
Ceto Romero <ceto.romero13@gmail.com>
2016-02-24 12:40:08 -0500

    fucking commit

--- listado.php
+++ listado.php
@@ -48,6 +48,50 @@ endif;
 
 }
 
+$id = $_GET['id'];
+
+print_r(getProductos($id));
+
+funtion getProductos($id)
+{
+
+
+	
+if ($id == 1):
+	return [
+				
+                 'nombre' => 'Zapatillas',
+                 'precio' => 200.00,
+                 'stock' => 2
+
+];
+
+
+       endif;
+
+if ($id== 2):
+	return [
+				'nombre' => 'Zapatillas',
+				'precio' =>  200.00,
+				'stock' => 2
+			
+
+
+];
+
+endif;
+
+if ($id == 3) :
+	return [
+				'nombre' => 'Polo',
+				'precio' => 80.00,
+				'stock' => 10,
+
+
+
+];
+	
+endif;	
 
 
 
@@ -58,14 +102,7 @@ endif;
 
 
 
-
-
-
-
-
-
-
-
+}
 
 
 
