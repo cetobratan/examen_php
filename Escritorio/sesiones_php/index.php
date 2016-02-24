54e0a41c21026dc03eeebe5bb89f1ba1adfa784c
Ceto Romero <ceto.romero13@gmail.com>
2016-02-24 11:40:36 -0500

    first commit

---
+++ Escritorio/sesiones_php/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<table border="1">
+		
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Nombre</th>
+				<th>Precio</th>
+				<th>Stock</th>
+				<th>Comprar</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>Zapatillas</td>
+				<td>200.00</td>
+				<td>2</td>
+				<td><a href="listado.php?id=1">AGREGAR</a></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Pelotas</td>
+				<td>60.00</td>
+				<td>5</td>
+				<td><a href="listado.php?id=2">AGREGAR</a></td>
+			</tr>
+			<tr>
+
+			<td>3</td>	
+			<td>Polo</td>
+			<td>80.00</td>
+			<td>10</td>	
+			<td><a href="listado.php?id=3">AGREGAR</a></td>
+
+
+
+			</tr>
+		</tbody>
+	</table>
+		
+
+
+</body>
+</html>
