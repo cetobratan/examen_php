2ac6d156076b87bac47b93d5eeda4e97266b30bc
ceto romero <ceto.romero13@gmail.com>
2016-03-02 16:45:07 -0800

    ccfc

--- Productos.php
+++ Productos.php
@@ -1,71 +1,88 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>ejercicio</title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+  <?php 
 
-examenfinal_start();
+          session_start();
 
+                  
+            $_id = $_GET['id'];
 
-$_id = $_GET['id'];
+            if(isset($_SESSION['carrito']))
+            {
+                if(isset($_SESSION['carrito'][md5($_id)])){
+                    
+                    $_SESSION['carrito'][md5($_id)]['stock']+=1;
+                }else{
+                   $_SESSION['carrito'][md5($_id)]= getProductos($_id); 
+                }
+                
+               
+            }else{
+             $_SESSION['carrito'][md5($_id)]= getProductos($_id);   
+            }
 
-if(isset($_EXAMENFINAL['carrito']))
-{
-    if(isset($_EXAMENFINAL['carrito'][md5($_id)])){
-        
-        $_EXAMENFINAL['carrito'][md5($_id)]['stock']+=1;
-    }else{
-       $_EXAMENFINAL['carrito'][md5($_id)]= getProductos($_id); 
-   EXAMENFINAL
-   
-}else{
- $_EXAMENFINAL['carrito'][md5($_id)]= getProductos($_id);   
-}
+            echo "<pre>";
 
+            print_r ($_SESSION);
 
+            //print_r(getProductos($_id));
 
-echo "<pre>";
-
-print_r ($_EXAMENFINAL);
-
-
-
-function getProductos($id)
-{
-    if($id == 1)
-    {
-        return[
-                'id'     => 1,
-                'nombre' => 'converse',
-                'Foto' => 'converse.jpg',
-                'descripcion' => 'Converse Chuck Taylor Black White High Top Canvas New in Box Sizes 3 5 to 12 ',
-                'precio' => 175.00,
-                'stock'  => 25
-              ];
-     }
-    
-      if($id==2)
-      {
-        return[
-                'id'     => 2,
+              function getProductos($id)
+              {
+                  if($id == 1)
+                  {
+                      return[
+                              'id'     => 1,
+                              'nombre' => 'converse',
+                                'Foto' => 'converse.jpg',
+                                'descripcion' => 'Converse Chuck Taylor Black White High Top Canvas New in Box Sizes 3 5 to 12 ',
+                
+                                   'stock'  => 25
+                               'precio' => 175.00,
+                            ];
+                  }
+                  
+                  if($id==2)
+                  {
+                      return[
+                              'id'     => 2,
                 'nombre' => 'Adidas',
                 'Foto' => 'adidasball.jpg',
                 'descripcion' => 'Very Rare Germany/Italy Teamgeist Ball',
                 'precio' => 125.00,
                 'stock'  => 50
-              ];
-      }
-    
-       if($id==3)
-      {
-        return[
-                'id'     => 3,
+                            ];
+                  }
+                  
+                  if($id==3)
+                  {
+                      return[
+                              'id'     => 3,
                 'nombre' => 'devil converse',
                 'Foto' => 'devilconverse.jpg'
                 'descripcion' => 'VVan Halen of Men B&w Winged Devil T-shirt Black',
                 'precio' => 85.00,
                 'stock'  => 10
-              ];
-      }    
-    return FALSE;
-    
-    
-}
+                            ];
+                  }    
+                  return FALSE;
+                  
+                  
+              }
 
  ?>
+
+ <h1><a href="index.php">Regresar</a></h1> 
+</body>
+</html>
+
+
+
+ 
